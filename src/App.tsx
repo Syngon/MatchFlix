d80fc45d97c662d106c52c1985ec023bd88a349d
Syngon <naotivecriatividadepraisso@gmail.com>
2021-11-16 23:16:07 -0300

    form

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,43 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>MATCHFLIX</h1>
+      <form>
+        <h2>AJUDAR PEDRO E LOUISE A ESCOLHER UM FILMES PORQUE SOMOS MUITO INDECISOS.</h2>
+
+        <select name="select">
+          <option value="valor1">Valor 1</option>
+          <option value="valor2" selected>Valor 2</option>
+          <option value="valor3">Valor 3</option>
+        </select>
+
+        <select name="select">
+          <option value="valor1">Valor 1</option>
+          <option value="valor2" selected>Valor 2</option>
+          <option value="valor3">Valor 3</option>
+        </select>
+
+        <select name="select">
+          <option value="valor1">Valor 1</option>
+          <option value="valor2" selected>Valor 2</option>
+          <option value="valor3">Valor 3</option>
+        </select>
+
+        <select name="select">
+          <option value="valor1">Valor 1</option>
+          <option value="valor2" selected>Valor 2</option>
+          <option value="valor3">Valor 3</option>
+        </select>
+
+        <select name="select">
+          <option value="valor1">Valor 1</option>
+          <option value="valor2" selected>Valor 2</option>
+          <option value="valor3">Valor 3</option>
+        </select>
+
+        <input type="submit" value="Escolher tudo pra nos!"/>
+
+      </form>
     </div>
   );
 }
